e23b66efade4daae1e8bfcb73fc3aada60662727
Jan CW Kroeze <thejcwk@gmail.com>
2016-04-19 22:59:48 +0200

    Basic window creation.

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,12 @@
+extern crate sdl2;
+
+use sdl2::event::Event;
+use sdl2::rect::Rect;
+use sdl2::pixels::Color;
+
 use std::fmt;
 
+#[allow(dead_code)]
 struct Vector {
     w: i32,
     x: i32,
@@ -7,6 +14,7 @@ struct Vector {
     z: i32,
 }
 
+#[allow(dead_code)]
 impl Vector {
     fn new() -> Vector {
         Vector {w: 0, x: 0, y: 0, z: 0}
@@ -19,20 +27,34 @@ impl fmt::Display for Vector {
     }
 }
 
+#[allow(unused_variables)]
 fn main() {
-    let axiom = "F";
-    let mut result = axiom;
-    let rules = [("F", "FF")];
-
-    for rule in &rules {
-        if result == rule.0 {
-            result = rule.1
+    let sdl = sdl2::init().unwrap();
+    let video = sdl.video().unwrap();
+    let mut window = video.window("Rust", 640, 480).build().unwrap();
+    window.show();
+    let mut event_pump = sdl.event_pump().unwrap();
+    {
+        let mut surface = window.surface_mut(&event_pump).unwrap();
+        let must_lock = surface.must_lock();
+        let rect = surface.clip_rect();
+        let color = Color::RGB(255, 255, 255);
+        surface.fill_rect(rect, color);
+    }
+    {
+        window.update_surface();
+    }
+    let mut done = false;
+    while !done {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::KeyDown {timestamp, window_id, keycode, scancode,
+                                keymod, repeat} => {
+                    done = true;
+                },
+                _ => ()
+            }
         }
     }
-
-    println!("Result: {}", result);
-
-    let v = Vector::new();
-    println!("Vector: {}", v);
 }
 
